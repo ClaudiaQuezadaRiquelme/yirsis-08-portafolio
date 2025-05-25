6bc3389c5a602156df669ae35e5d49c3249d87ef
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2025-05-25 13:48:51 -0400

    Proyectos.data.ts

---
+++ src/app/shared/data/Proyectos.data.ts
@@ -0,0 +1,34 @@
+export const proyectos: any[] = [
+  {
+    nombre: 'Contador',
+    img: '/assets/contador.png',
+    tecnologias: ['Angular Framework', 'CSS', 'PWA'],
+    descripcion:
+      'Aplicacion de contador aprendiendo Angular y a manejar eventos y reactividad',
+    url: 'https://yirsis-01-contador.netlify.app/',
+  },
+  {
+    nombre: 'Pokeapp',
+    img: '/assets/pokeapp.png',
+    tecnologias: ['Angular Framework', 'Angular Material', 'PWA'],
+    descripcion:
+      'Aplicacion de pokemon usando Angular Material para los estilos',
+    url: 'https://yirsis-03-pokeapp.netlify.app/',
+  },
+  {
+    nombre: 'Listado de Tareas',
+    img: '/assets/tareas.png',
+    tecnologias: ['Angular Framework', 'Bulma CSS'],
+    descripcion:
+      'Aplicacion de listado de tareas usando Bulma CSS como Framework para los estilos',
+    url: 'https://yirsis-02-tareas.netlify.app/',
+  },
+  {
+    nombre: 'Pipes App',
+    img: '/assets/pipes.png',
+    tecnologias: ['Angular Framework', 'Bootstrap 5'],
+    descripcion:
+      'Aplicacion de listado de tareas usando Bootstrap 5 como Framework para los estilos',
+    url: 'https://yirsis-04-pipesapp.netlify.app/',
+  },
+];
